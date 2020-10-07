74a6df5690f557e7247ffe66f92ecafc193dff7f
huonggiangbui <bui.huonggiang4ahht@gmail.com>
2020-10-08 00:22:55 +0700

    Updates

---
+++ precache-manifest.acc887f9ada4247d63e6fae850940ed4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ada22d46b22f1134ff938ec2ef23b5c",
+    "url": "/EasyQuiz/index.html"
+  },
+  {
+    "revision": "ec38a6ac58a775a6cced",
+    "url": "/EasyQuiz/static/css/2.c37a399a.chunk.css"
+  },
+  {
+    "revision": "00085086950c48c8b0ae",
+    "url": "/EasyQuiz/static/css/main.da609efd.chunk.css"
+  },
+  {
+    "revision": "ec38a6ac58a775a6cced",
+    "url": "/EasyQuiz/static/js/2.3e106efe.chunk.js"
+  },
+  {
+    "revision": "3ceb8c17f560e6682fd3523301e674b3",
+    "url": "/EasyQuiz/static/js/2.3e106efe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00085086950c48c8b0ae",
+    "url": "/EasyQuiz/static/js/main.44657600.chunk.js"
+  },
+  {
+    "revision": "f101550ab23d0915338e",
+    "url": "/EasyQuiz/static/js/runtime-main.a5748810.js"
+  },
+  {
+    "revision": "ff6de78513bc26761d67103231b11836",
+    "url": "/EasyQuiz/static/media/image1.ff6de785.jpg"
+  },
+  {
+    "revision": "ab22cffbd01af3fe40bb6205ebd0e676",
+    "url": "/EasyQuiz/static/media/image2.ab22cffb.jpg"
+  },
+  {
+    "revision": "f5708c16a6d320937e32fe081cd72231",
+    "url": "/EasyQuiz/static/media/image3.f5708c16.jpg"
+  }
+]);
